65095c0af493f1d6dd409736dbe42fe952592edb
Andrey Sharton <sharton.web@gmail.com>
2025-09-22 15:46:45 +0600

    Separate templates, add post_detail, add urls for all posts

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+
+# Post.objects.all()
+# Post.objects.get(id=1)
+# Post.objects.filter()
+# author = User.objects.get(username='pushkin')
+# Filters :
+    # contains 
+    # exact =
+    # startwith
+    # endwith 
+    # gt >
+    # gte >=
+    # lt < 
+    # lte <=
