297a9be038b5619315b5c637dcfa1ab7f6a73183
Andrey Sharton <sharton.web@gmail.com>
2025-10-03 13:17:40 +0600

    Add CBV

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.post_list, name='post_list'),
-  path('<int:pk>/', views.post_detail, name='post_detail'),
-  path('new/', views.post_create, name='post_create'),
-  path('<int:pk>/edit', views.post_update, name='post_update'),
+  path('', views.PostListView.as_view(), name='post_list'),
+  path('<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+  path('new/', views.PostCreateView.as_view(), name='post_create'),
+  path('<int:pk>/edit', views.PostUpdateView.as_view(), name='post_update'),
+  path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,44 +1,72 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
 from django.contrib.auth.models import User
 from .models import Post
 from .forms import PostForm
+from django.urls import reverse_lazy
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+class PostListView(ListView):
+    model = Post
+    context_object_name = "posts"
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = "post"
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy("post_list")
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy("post_list")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')
+
+# def post_list(request):
+#     posts = Post.objects.all()
+#     return render(request, 'blog/post_list.html', {'posts': posts})
+
+# def post_detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, 'blog/post_detail.html', {'post': post})
+
+# def post_create(request):
+#     if request.method == "POST":
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.save()
+#             form.save_m2m()
+#             return redirect('post_list')
+#     else:
+#         form = PostForm()
+#         return render(request, 'blog/post_form.html', {'form': form})
 
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
-
-def post_create(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            form.save_m2m()
-            return redirect('post_list')
-    else:
-        form = PostForm()
-        return render(request, 'blog/post_form.html', {'form': form})
-
-
-def post_update(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            # post = form.save(commit=False)
-            # post.author = request.user
-            # post.save()
-            form.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-        return render(request, 'blog/post_form.html', {'form': form})
+# def post_update(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     if request.method == "POST":
+#         form = PostForm(request.POST, instance=post)
+#         if form.is_valid():
+#             # post = form.save(commit=False)
+#             # post.author = request.user
+#             # post.save()
+#             form.save()
+#             return redirect('post_detail', pk=post.pk)
+#     else:
+#         form = PostForm(instance=post)
+#         return render(request, 'blog/post_form.html', {'form': form})
 
 # C_R_UD
 
