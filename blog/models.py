44e3037081e339f7d6807caa83c8526a153cdfe4
Andrey Sharton <sharton.web@gmail.com>
2025-10-17 15:24:03 +0600

    Add Image

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     categories = models.ManyToManyField(Category, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
